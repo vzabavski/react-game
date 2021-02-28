f691fd1f0ce7f92dadbca0e07fa790b934ba4016
vzabavski <vzabavski123@gmail.com>
2021-02-28 13:19:05 +0300

    fix: change mobile version

--- react-game/src/components/Gamefield/Gamefield.tsx
+++ react-game/src/components/Gamefield/Gamefield.tsx
@@ -112,7 +112,7 @@ export class Gamefield extends React.Component<{addStep: ()=> void, sizeClass?: 
                 <div className={`gamefield-wrapper ${options.style} ${options.sizeClass}`} >
                 { creatCells() }
                 </div>
-                <ModalWindow visibility={this.state.visibility} children={<Win />} onClose={() => console.log('close')} type='win' /> 
+                <ModalWindow visibility={''} children={<Win />} onClose={() => console.log('close')} type='win' /> 
             </>
             
         )
